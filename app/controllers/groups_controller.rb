e355ef81b16100f0c2dc94b5c1c7da022fc29a96
anderCM <andercm15@gmail.com>
2023-06-06 09:55:47 -0500

    Group views and custom tailwind style added

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,29 @@
 class GroupsController < ApplicationController
-  def index; end
+  load_and_authorize_resource
+  def index
+    @groups = Group.where(user: current_user).order(created_at: :desc)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.name = params[:group][:name].capitalize
+    @group.user = current_user
+
+    begin
+      @group.save
+      redirect_to groups_path, notice: 'Group created successfully'
+    rescue StandardError
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { minimum: 2, maximum: 150 }
 
   has_many :groups
