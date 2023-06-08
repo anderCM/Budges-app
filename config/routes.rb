b65c9cb5411f2fe7bfefb8f6755692540975a204
anderCM <andercm15@gmail.com>
2023-06-07 22:05:10 -0500

    Routes and views modified to match 'categories' and 'transactions'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root :to => redirect('groups')
+  root :to => redirect('categories')
 
   resources :users, only: [:index, :show]
-  resources :groups, only: [:index, :show, :new, :create] do
-    resources :entities, only: [:index, :show, :new, :create]
+  resources :groups, path: 'categories', only: [:index, :show, :new, :create] do
+    resources :entities, path: 'transactions', only: [:index, :show, :new, :create]
   end
 end
